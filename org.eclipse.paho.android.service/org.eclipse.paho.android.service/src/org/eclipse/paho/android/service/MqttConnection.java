96c3ed40a53eb6a921c5664b5ed1df0132425d31
M Nanda Rimansyah <nanda@netzme.id>
2015-12-22 10:12:43 +0700

    put persistence on internal storage & remove FileLock
    https://trello.com/c/XwfyKRX7

--- org.eclipse.paho.android.service/org.eclipse.paho.android.service/src/org/eclipse/paho/android/service/MqttConnection.java
+++ org.eclipse.paho.android.service/org.eclipse.paho.android.service/src/org/eclipse/paho/android/service/MqttConnection.java
@@ -214,25 +214,8 @@ class MqttConnection implements MqttCallback {
 				
 		try {
 			if (persistence == null) {
-				// ask Android where we can put files
-				File myDir = service.getExternalFilesDir(TAG);
-
-				if (myDir == null) {
-					// No external storage, use internal storage instead.
-					myDir = service.getDir(TAG, Context.MODE_PRIVATE);
-					
-					if(myDir == null){
-						//Shouldn't happen.
-						resultBundle.putString(
-								MqttServiceConstants.CALLBACK_ERROR_MESSAGE,
-								"Error! No external and internal storage available");
-						resultBundle.putSerializable(
-								MqttServiceConstants.CALLBACK_EXCEPTION, new MqttPersistenceException());
-						service.callbackToActivity(clientHandle, Status.ERROR,
-								resultBundle);
-						return;
-					}
-				}
+				// use internal storage
+				File myDir = service.getDir(TAG, Context.MODE_PRIVATE);
 
 				// use that to setup MQTT client persistence storage
 				persistence = new MqttDefaultFilePersistence(
